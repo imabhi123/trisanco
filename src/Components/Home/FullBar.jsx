65c3a57c930c7d502cd39e4c9ad8fdd329751a67
Harry <kumarabhishek13909@gmail.com>
2024-05-16 19:33:23 +0530

    Update 16/05/24

--- src/Components/Home/FullBar.jsx
+++ src/Components/Home/FullBar.jsx
@@ -1,10 +1,30 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import getImg from '../../Assets/img/git_in_touch.gif'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCross, faXmark } from '@fortawesome/free-solid-svg-icons'
 
-const FullBar = () => {
+const FullBar = ({ setFlag }) => {
   return (
     <div className='fullbar_main_component'>
-      <div className='side_fullbar'></div>
+      <div className='side_fullbar'>
+        <div className="logo_fullbar">
+          <Link href={'/'}>TRISANCO</Link>
+        </div>
+        <div className="socials_fullbar">
+          <p className="socials_fullbar_heading">Follow us</p>
+          <Link href={'https://www.linkedin.com/company/trisanco-infotech-pvt-ltd/'} className='socials_fullbar_socials'>Linkedin</Link>
+          <Link href={'https://www.instagram.com/trisanco_infotech?igsh=MW9vOTJ2OXgzMnQ1'} className='socials_fullbar_socials'>Instagram</Link>
+          <Link href={'https://www.facebook.com/share/ELW2ZpLJBqXnQStd/?mibextid=qi2Omg'} className='socials_fullbar_socials'>Facebook</Link>
+        </div>
+        <hr />
+        <div className="side_linkss">
+          <Link href={'/about-us'} className="links">About Us</Link>
+          <Link href={'/blogs'} className="links">Blogs</Link>
+          <Link href={'/contact'} className="links">Contact Us</Link>
+        </div>
+      </div>
       <hr />
       <div className='main_fullbar'>
         <Link href={'/'} className="links">Home</Link>
@@ -14,8 +34,20 @@ const FullBar = () => {
         <Link href={'/contact'} className="links">Contact Us</Link>
       </div>
       <hr />
-      <div className='right_fullbar'></div>
-      
+      <div className='right_fullbar'>
+        <Image src={getImg} height={350} width={250} />
+        <div className="get_in_touch_fullbar">
+          <p className="getInTouch_title">Get in touch</p>
+          <Link href={'/'} className="getInTouch_desc">+91 07554506621</Link>
+          <Link href={'/'} className="getInTouch_desc">trisancoinfotech@gmail.com</Link>
+          <Link href={'/'} className="getInTouch_desc">117, Shri Ram Colony, Near SMH Hospital, Hoshangabad Road, Bhopal,Madhya Pradesh</Link>
+
+        </div>
+      </div>
+
+      <div style={{ position: 'absolute', top: "50px", right: '50px', background: 'black', height: '80px', width: '80px', borderRadius: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'white', cursor: 'pointer' }}>
+        <FontAwesomeIcon onClick={() => setFlag(false)} color='white' icon={faXmark} style={{ fontSize: '40px' }} />
+      </div>
     </div>
   )
 }
--- src/Components/Home/SideNav.jsx
+++ src/Components/Home/SideNav.jsx
@@ -3,11 +3,15 @@ import './css/home.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import FullBar from './FullBar'
+import Link from 'next/link'
 
 const SideNav = ({setFlag}) => {
 
   return (
     <div className='sideNav_main_div' style={{zIndex:'9',position:''}}>
+      <div className="logo_fullbar" style={{position:'relative',left:'-30%',bottom:'20px'}} >
+          <Link href={'/'} style={{fontSize:'42px',marginLeft:'auto'}} >TRISANCO</Link>
+        </div>
       <FontAwesomeIcon icon={faBars} onClick={()=>setFlag(true)} style={{transform:'rotate(90deg)',cursor:'pointer'}} />
       
     </div>
